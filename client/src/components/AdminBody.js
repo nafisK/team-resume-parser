bd308879bf0a6809b8080f5231dc535daceb3b9b
Nafis Khan <nafisrizwank@gmail.com>
2022-08-13 19:59:50 -0400

    fixed dashboard spacing

--- client/src/components/AdminBody.js
+++ client/src/components/AdminBody.js
@@ -21,11 +21,12 @@ export default function AdminBody() {
   return (
     <div className='container mx-auto mt-40 '>
       <div className='grid grid-cols-3 gap-4'>
-        <div className='w-full px-4 py-5 bg-white rounded-lg shadow text-[#424B5A] text-align-center'>
-          <Pie/>
-          <LineGraph/>
+        <div className='w-full px-4 py-5 bg-white rounded-lg shadow text-[#424B5A] text-align-center '>
+          <Pie />
+          <hr className='mb-20' />
+          <LineGraph />
         </div>
-        
+
         <div className='w-full px-4 py-5 bg-white rounded-lg shadow col-span-2 '>
           <Table />
         </div>
--- client/src/components/Pie.js
+++ client/src/components/Pie.js
@@ -1,26 +1,29 @@
-import React from "react";
-import { Chart } from "react-google-charts";
+import React from 'react'
+import { Chart } from 'react-google-charts'
 
 export const data = [
-  ["Skill", "Applicants with Skill"],
-  ["Python", 550],
-  ["C++", 200],
-  ["Java", 150],
-  ["React.js", 400],
-  ["UI/UX", 100],
-];
+  ['Skill', 'Applicants with Skill'],
+  ['Python', 550],
+  ['C++', 200],
+  ['Java', 150],
+  ['React.js', 400],
+  ['UI/UX', 100],
+]
 
+export const options = {
+  title: 'Skill Distribution',
+}
 
 export function Pie() {
   return (
-    <div>
-    <h3 className="applicants">SKILLS</h3>
-    <Chart
-      chartType="PieChart"
-      data={data}
-      width={300}
-      height={200}
-    />
+    <div className='flex flex-col'>
+      <h3 className='applicants'>SKILLS</h3>
+      <Chart
+        chartType='PieChart'
+        data={data}
+        options={options}
+        height={'400px'}
+      />
     </div>
-  );
+  )
 }
